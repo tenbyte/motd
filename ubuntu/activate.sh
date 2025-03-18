cc3b66aac2793199f8100e5cccdb5a50858f1728
Wiesty <106901362+wiesty@users.noreply.github.com>
2025-03-18 11:25:39 +0100

    Update activate.sh

--- ubuntu/activate.sh
+++ ubuntu/activate.sh
@@ -7,19 +7,20 @@ MOTD_SCRIPT="/etc/update-motd.d/99-tenbyte-motd"
 
 if [[ -f "$MOTD_SCRIPT" ]]; then
     echo "Existing TENBYTE MOTD found. Overwriting..."
-    sudo wget -O "$MOTD_SCRIPT" "$MOTD_URL"
 else
-    echo "No TENBYTE MOTD found. Leaving existing MOTD unchanged."
-    exit 0
+    echo "No TENBYTE MOTD found. Adding a new one..."
 fi
+
+# Download oder Überschreiben des MOTD
+sudo wget -O "$MOTD_SCRIPT" "$MOTD_URL"
 
 echo "Making the TENBYTE MOTD script executable..."
 sudo chmod +x "$MOTD_SCRIPT"
 
 echo "Disabling Ubuntu's default MOTD messages..."
-sudo chmod -x /etc/update-motd.d/00-header
-sudo chmod -x /etc/update-motd.d/10-help-text
-sudo chmod -x /etc/update-motd.d/50-motd-news
+sudo chmod -x /etc/update-motd.d/00-header 2>/dev/null
+sudo chmod -x /etc/update-motd.d/10-help-text 2>/dev/null
+sudo chmod -x /etc/update-motd.d/50-motd-news 2>/dev/null
 
 echo "Applying the new MOTD..."
 run-parts /etc/update-motd.d/
