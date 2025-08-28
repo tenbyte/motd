5292a22e8b636a95fb0b6735801c96fe713f2e9b
wiesty <106901362+wiesty@users.noreply.github.com>
2025-08-28 23:48:28 +0200

    added ip.tenbyte.dev

--- mac/tenbyte-motd.sh
+++ mac/tenbyte-motd.sh
@@ -7,7 +7,8 @@ RESET="\033[0m"
 
 DISTRO=$(sw_vers -productName)" "$(sw_vers -productVersion)
 IP=$(ipconfig getifaddr en0)
-WANIP4=$(curl -s https://ipinfo.io/ip)
+WANIP4=$(curl -s https://ipv4.tenbyte.dev/plain)
+WANIP6=$(curl -s https://ipv6.tenbyte.dev/plain)
 UPTIME=$(uptime | awk -F'( |,|:)+' '{print $6 "h " $7 "m"}')
 LOAD=$(uptime | awk -F'load averages?: ' '{print $2}')
 
@@ -22,8 +23,8 @@ echo -e "${BOLD}${CYAN}       POWERED BY TENBYTE ${RESET}\n"
 
 echo -e "${WHITE}-----------------------------------------${RESET}"
 echo -e "${CYAN}  Hostname:   ${WHITE}$(scutil --get LocalHostName)${RESET}"
-echo -e "${CYAN}  LAN IP:     ${WHITE}${IP:-N/A}${RESET}"
 echo -e "${CYAN}  WAN IPv4:   ${WHITE}${WANIP4:-N/A}${RESET}"
+echo -e "${CYAN}  WAN IPv6:   ${WHITE}${WANIP6:-N/A}${RESET}"
 echo -e "${CYAN}  Distro:     ${WHITE}${DISTRO}${RESET}"
 echo -e "${CYAN}  Uptime:     ${WHITE}${UPTIME}${RESET}"
 echo -e "${CYAN}  Load Avg:   ${WHITE}${LOAD}${RESET}"
