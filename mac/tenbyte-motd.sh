407902f639fc81ab984395ea9c5ef12eb03236e8
Wiesty <106901362+wiesty@users.noreply.github.com>
2025-11-07 11:55:31 +0100

    Update IP retrieval methods

--- mac/tenbyte-motd.sh
+++ mac/tenbyte-motd.sh
@@ -6,9 +6,9 @@ BOLD="\033[1m"
 RESET="\033[0m"
 
 DISTRO=$(sw_vers -productName)" "$(sw_vers -productVersion)
-LOCALIP4=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}' | head -n 1)
-WANIP4=$(curl -s --connect-timeout 1 --max-time 2 https://ipv4.tenbyte.dev/plain || echo "N/A")
-WANIP6=$(curl -s --connect-timeout 1 --max-time 2 https://ipv6.tenbyte.dev/plain || echo "N/A")
+LOCALIP4=$(ipconfig getifaddr en0 2>/dev/null || ipconfig getifaddr en1 2>/dev/null)
+WANIP4=$(curl -s --connect-timeout 1 --max-time 2 https://ipv4.tenbyte.dev/plain)
+WANIP6=$(curl -s --connect-timeout 1 --max-time 2 https://ipv6.tenbyte.dev/plain)
 
 UPTIME=$(uptime | awk -F'( |,|:)+' '{print $6 "h " $7 "m"}')
 LOAD=$(uptime | awk -F'load averages?: ' '{print $2}')
@@ -24,9 +24,11 @@ echo -e "${BOLD}${CYAN}       POWERED BY TENBYTE ${RESET}\n"
 
 echo -e "${WHITE}-----------------------------------------${RESET}"
 echo -e "${CYAN}  Hostname:   ${WHITE}$(scutil --get LocalHostName)${RESET}"
-echo -e "${CYAN}  WAN IPv4:   ${WHITE}${WANIP4:-N/A}${RESET}"
-echo -e "${CYAN}  WAN IPv6:   ${WHITE}${WANIP6:-N/A}${RESET}"
-echo -e "${CYAN}  Local IPv4: ${WHITE}${LOCALIP4:-N/A}${RESET}" 
+
+[ -n "$WANIP4" ] && echo -e "${CYAN}  WAN IPv4:   ${WHITE}${WANIP4}${RESET}"
+[ -n "$WANIP6" ] && echo -e "${CYAN}  WAN IPv6:   ${WHITE}${WANIP6}${RESET}"
+[ -n "$LOCALIP4" ] && echo -e "${CYAN}  Local IPv4: ${WHITE}${LOCALIP4}${RESET}"
+
 echo -e "${CYAN}  Distro:     ${WHITE}${DISTRO}${RESET}"
 echo -e "${CYAN}  Uptime:     ${WHITE}${UPTIME}${RESET}"
 echo -e "${CYAN}  Load Avg:   ${WHITE}${LOAD}${RESET}"
